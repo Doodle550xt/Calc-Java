569d2e2931c50a1e72af05b2416ffef73a7ee88c
Doodle550xt <144170664+Doodle550xt@users.noreply.github.com>
2023-09-09 19:23:55 -0600

    Update algoritmo.py
    Me faltaba graficar el de 2**x y agregar los títulos de cada gráfico

--- algoritmo.py
+++ algoritmo.py
@@ -9,19 +9,28 @@ y = x**2
 y1 = np.log(x)
 y2 = x
 y3 = x*(np.log(x))
-y4 = x**2
+y4 = [1]*100
+y5 = 2**x
 
-# Graficamos la función
-plt.plot(x, y)
-plt.plot(x, y1)
-plt.plot(x, y2)
-plt.plot(x, y3)
-plt.plot(x, y4)
+# Graficamos la función y añadimos etiquetas
+plt.plot(x, y, label='y = x^2')
+plt.plot(x, y1, label='y = log(x)')
+plt.plot(x, y2, label='y = x')
+plt.plot(x, y3, label='y = x*log(x)')
+plt.plot(x, y4, label='y = 1')
+plt.plot(x, y5, label='y = 2^x')
 
 # Añadimos etiquetas y título
 plt.xlabel('Eje x')
 plt.ylabel('Eje y')
-plt.title('Gráfico de y = x^2')
+plt.title('Gráfico de varias funciones')
+
+# Establecemos los límites del eje Y
+plt.ylim(0, 8000)
+
+# Añadimos una leyenda
+plt.legend()
 
 # Mostramos el gráfico
 plt.show()
+
